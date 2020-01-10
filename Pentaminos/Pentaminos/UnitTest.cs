23c7f15c85563c260c81be48758f4b49a800a4d1
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2020-01-10 10:31:44 +0100

    Adding Test class config

---
+++ Pentaminos/Pentaminos/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentaminos
+{
+    class UnitTest
+    {
+        [Test]
+        public void TestDescription()
+        {
+            Assert.That(Program.Description.Length,Is.GreaterThan(0));
+        }
+
+    }
+}
