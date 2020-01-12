aa79a1759884fee4c52145359ba3852518bb3224
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2020-01-12 08:28:04 +0100

    Test list pentaminos

---
+++ Pentaminos/Pentaminos/FabriqueDePentaminos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentaminos
+{
+    class FabriqueDePentaminos
+    {
+        static public List<Pentamino> ListeDePentaminos()
+        {
+            List<Pentamino> liste = new List<Pentamino>();
+          
+            for (int i=0; i<63; i++)
+            {
+                liste.Add(new Pentamino());
+            }
+
+            return liste;
+        }
+    }
+}
---
+++ Pentaminos/Pentaminos/TestListePentaminos.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pentaminos
+{
+    [TestFixture]
+    public class TestListePentaminos
+    {
+        [Test]
+        public void TestTotalPentaminos()
+        {
+            Assert.That(FabriqueDePentaminos.ListeDePentaminos().Count,Is.EqualTo(63));
+        }
+    }
+}
